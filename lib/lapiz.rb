d76a23e08f1a8b25d7e4da6f5fa3a6ad2af207f3
Ajith Hussain <csy0013@googlemail.com>
2015-10-03 16:16:37 +1000

    Added DSL methods 'group' and 'get'. WIP.

--- lib/lapiz.rb
+++ lib/lapiz.rb
@@ -1,10 +1,34 @@
 require "lapiz/version"
 
+module Lapiz
+  def group(name, &block)
+    describe(name) do
+      config = YAML.load(IO.read("config.yml"))
+      metadata[:lapiz] = {name: name}
+      metadata[:base_uri] = config["server"]["base_uri"]
+      instance_eval &block
+    end
+  end
+
+  def get(path, &block)
+    it "tests action '#{path}'" do
+      @response = HTTParty.get(path)
+      block.call
+    end
+  end
+
+  def response
+    @response
+  end
+end
+
 RSpec.configure do |config|
-  configure.before(:suite) do
+  config.before(:suite) do
     # Create or clear api-docs folder
   end
 
   config.after(:example) do |example|
   end
 end
+
+include Lapiz
