203b083b95bb0731caa45a85f008a76ea7bbe3db
Ajith Hussain <csy0013@googlemail.com>
2015-10-03 22:03:46 +1000

    Passing group to it so as to access stuff

--- lib/lapiz.rb
+++ lib/lapiz.rb
@@ -4,21 +4,18 @@ module Lapiz
   def group(name, &block)
     describe(name) do
       config = YAML.load(IO.read("config.yml"))
-      metadata[:lapiz] = {name: name}
       metadata[:base_uri] = config["server"]["base_uri"]
       instance_eval &block
     end
   end
 
   def get(path, &block)
-    it "tests action '#{path}'" do
-      @response = HTTParty.get(path)
+    it "tests action '#{path}'", self  do |group|
+      expect {
+        @response = HTTParty.get(group.metadata[:base_uri] + path)
+      }.to_not raise_error
       block.call
     end
-  end
-
-  def response
-    @response
   end
 end
 
