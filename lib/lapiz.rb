2fb8287d933d3bf41fc207cf78080997b4ab91ae
Ajith Hussain <csy0013@googlemail.com>
2015-10-05 00:14:35 +1100

    Preliminary generation of api doc

--- lib/lapiz.rb
+++ lib/lapiz.rb
@@ -1,27 +1,58 @@
 require "lapiz/version"
+require "fileutils"
+
+class String
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+end
 
 module Lapiz
   def group(name, &block)
     describe(name) do
       config = YAML.load(IO.read("config.yml"))
       metadata[:base_uri] = config["server"]["base_uri"]
+      metadata[:group_name] = name
+
+      FileUtils.mkdir_p("api_docs")
+      File.open("api_docs/#{name.gsub(/[^a-zA-Z_]+/,'_').underscore}.md", "w+") do |fp|
+        fp.puts "# Group #{name}"
+      end
+
       instance_eval &block
     end
   end
 
-  def get(path, &block)
+  def get(path, params = {}, &block)
+    http_call(:get, path, params, &block)
+  end
+
+  def post(path, params, &block)
+    http_call(:post, path, params, &block)
+  end
+
+  def http_call(method, path, params, &block)
     it "tests action '#{path}'", self  do |group|
       expect {
-        @response = HTTParty.get(group.metadata[:base_uri] + path)
+        @response = HTTParty.send(method, group.metadata[:base_uri] + path, params)
       }.to_not raise_error
-      block.call
+      instance_eval "def response;@response;end"
+      instance_eval &block
+
+      group_name = group.metadata[:group_name]
+      File.open("api_docs/#{group_name.gsub(/[^a-zA-Z_]+/,'_').underscore}.md", "a+") do |fp|
+        fp.puts "# #{method.to_s.upcase} #{path}"
+      end
     end
   end
 end
 
 RSpec.configure do |config|
-  config.before(:suite) do
-    # Create or clear api-docs folder
+  config.after(:suite) do
   end
 
   config.after(:example) do |example|
